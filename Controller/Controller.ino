c32392a176c92403da81e03a3cc987cf5e488576
Joshua Castor <jcasto3@uic.edu>
2017-11-20 10:45:52 -0600

    Add basic throttle IO code

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -4,13 +4,25 @@
 // Pin 3 --> Bluetooth RX
 SoftwareSerial btModule(2, 3);
 
+const char scrambledAlphabet[26] = "abCdEFghIJkLmnOpQRsTuvWXyz";
+
+// analog stick setup reference: https://brainy-bits.com/blogs/tutorials/arduino-joystick-tutorial
+const int analogX_pin = 4; //VRy
+const int analogY_pin = 3; //VRx
+const int analogSwitch_pin = 4;
+
+//possible range of analog stick values
+#define STICK_MIN 0
+#define STICK_MAX 1023
+
 #define CONNECTION_RATE 9600 //rate of servant module
 
 void setup() {
   Serial.begin(9600);
   btModule.begin(CONNECTION_RATE);
 
-  // other init code go here  
+  pinMode(analogSwitch_pin, INPUT);
+  digitalWrite(analogSwitch_pin, LOW);
   
   Serial.println("Ready");
 }
@@ -35,9 +47,41 @@ void sendFromSerial(){
   }
 }
 
+//reference: https://www.arduino.cc/reference/en/language/functions/math/map/
+//scale to 0-1 scale
+String scaleAnalog(int value){
+  int mappedValue = map(value, STICK_MIN, STICK_MAX, 0, 10);
+  return String(mappedValue);
+}
+
+//encode string from regular numbers to scrambled alphabet
+String encodeNumbers(String input){
+  String s = "";
+  const int len = input.length();
+  for(int i = 0; i < len; ++i){
+    char currentChar = input.charAt(i);
+
+    //replace number with character at index
+    if(isDigit(currentChar)){
+      currentChar = scrambledAlphabet[(int)currentChar - '0'];
+    }
+
+    Serial.print(String(input.charAt(i)) + "->" + currentChar + "| ");
+
+    s += currentChar;
+  }
+  return s;
+}
+
 void loop() {
   sendFromSerial();
 
-  //other input handlers go here
+  String x = scaleAnalog(analogRead(analogX_pin));
+  String y = scaleAnalog(analogRead(analogY_pin));
+  String throttleMsg = encodeNumbers("T" + x + "," + y);
 
+  Serial.println("X: " + x + ", Y: " + y + "| encoded: " + throttleMsg);
+  sendString(throttleMsg);
+  
+  delay(50);
 }
--- Hovercraft/Hovercraft.ino
+++ Hovercraft/Hovercraft.ino
@@ -6,6 +6,8 @@ SoftwareSerial btModule(2, 3);
 
 #define CONNECTION_RATE 9600 //rate of servant module
 
+const String numberAlphabet = "abCdEFghIJ";
+
 void setup() {
   Serial.begin(38400);
   btModule.begin(CONNECTION_RATE);
@@ -28,12 +30,41 @@ String readModule(){
   return s;
 }
 
+//decode string from scrambled alphabet to regular numbers
+String decodeNumbers(String input){
+  String s = "";
+  const int len = input.length();
+  for(int i = 0; i < len; ++i){
+    char currentChar = input.charAt(i);
+    int index = numberAlphabet.indexOf(currentChar);
+    //  convert input to numbers
+    if(index > -1){
+      currentChar = (char)index + '0';
+    }
+    s += currentChar;
+  }
+  return s;
+}
+
+void handleThrottle(String input){
+  input = decodeNumbers(input);
+  int commaIndex = input.indexOf(',');
+  //x: -5 is left, 5 is right
+  //y: 5 is up, -5 is down
+  int x = input.substring(0, commaIndex).toInt() - 5;
+  int y = input.substring(commaIndex+1).toInt() - 5;
+
+  Serial.println("Throttle params: " + input + "-> " + x + "," + y);
+}
+
 String input;
 void loop() {
   input = readModule();
 
   if(input.length() > 0){
-    // input handling goes here
+    if(input.charAt(0) == 'T'){ //throttle handling message
+      handleThrottle(input.substring(1));
+    }
   }
 
   // input independent code goes here
